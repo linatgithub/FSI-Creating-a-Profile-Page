0e37bd53cb0e85bbcb76a0ecf57b7ffbde99c725
Lin <linlincc@gmail.com>
2022-08-01 01:59:30 -0400

    update dog details

--- index.js
+++ index.js
@@ -11,12 +11,35 @@ dogContent.setAttribute('class', 'dog-content')
 content.append(dogContent)
 
 let dogImage = document.createElement('img')
-dogImage.setAttribute('class', 'dog-image')
+dogImage.setAttribute('class', 'dog-image') 
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
 
+let description = document.createElement ('h3')
+// description.setAttribute('class', 'dog-content')
+description.textContent = 'Description:'
+dogDetails.append(description)
 
+let details = document.createElement ('p')
+details.textContent = "This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals."
+dogDetails.append (details)
+
+let feedingHeader = document.createElement ('h3')
+feedingHeader.textContent = 'Feeding Times:'
+dogDetails.append (feedingHeader)
+
+let feedingTimes = document.createElement ('ul')
+dogDetails.append (feedingTimes)
+let time1 = document.createElement ('li')
+time1.textContent = "9:00 am"
+feedingTimes.append (time1)
+let time2 = document.createElement ('li')
+time2.textContent = "12:00 pm"
+feedingTimes.append (time2)
+let time3 = document.createElement ('li')
+time3.textContent = "5:00 pm"
+feedingTimes.append (time3)
